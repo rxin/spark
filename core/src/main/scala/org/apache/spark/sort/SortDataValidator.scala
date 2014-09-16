4d78e8f2e7b558a0fb5e50317b5936ce1fa77145
Reynold Xin <rxin@apache.org>
2014-09-16 15:21:11 -0700

    new validator.

--- core/src/main/scala/org/apache/spark/sort/SortDataValidator.scala
+++ core/src/main/scala/org/apache/spark/sort/SortDataValidator.scala
@@ -2,6 +2,8 @@ package org.apache.spark.sort
 
 import java.io.{FilenameFilter, FileOutputStream, RandomAccessFile, File}
 
+import scala.sys.process._
+
 import org.apache.spark.{TaskContext, Partition, SparkConf, SparkContext}
 
 
@@ -19,45 +21,65 @@ object SortDataValidator {
     val hosts = Sort.readSlaves()
     val numEBS = UnsafeSort.NUM_EBS
 
-    val output = sc.parallelize(1 to hosts.length, hosts.length).mapPartitions { iter =>
+    val output: Array[(Int, String, String)] =
+      sc.parallelize(1 to hosts.length, hosts.length).mapPartitions { iter =>
+        (0 until numEBS).iterator.flatMap { ebs =>
+          val host = "hostname".!!
 
-      (0 until numEBS).iterator.flatMap { ebs =>
-        val baseFolder = new File(s"/vol$ebs/sort-${sizeInGB}g-$numParts-out")
-        val files = baseFolder.listFiles(new FilenameFilter {
-          override def accept(dir: File, filename: String): Boolean = {
-            filename.endsWith(".dat")
+          val baseFolder = new File(s"/vol$ebs/sort-${sizeInGB}g-$numParts-out")
+          val files: Array[File] = baseFolder.listFiles(new FilenameFilter {
+            override def accept(dir: File, filename: String): Boolean = {
+              filename.endsWith(".dat")
+            }
+          })
+
+          if (files != null) {
+            files.iterator.map { file: File =>
+              val outputFile = file.getAbsolutePath
+              val partIndex = "(\\d+)\\.dat".r.findFirstIn(outputFile).get.replace(".dat", "").toInt
+              (partIndex, host, outputFile)
+            }
+          } else {
+            Seq.empty[(Int, String, String)]
           }
-        })
-
-        files.iterator.map { file: File =>
-          val outputFile = file.getAbsolutePath
-          val checksumOutput = outputFile.replace(".dat", ".sum")
-          val cmd = s"/root/gensort/64/valsort -o $checksumOutput $outputFile"
-
-          val (exitCode, stdout, stderr) = Sort.runCommand(cmd)
-
-          val checksumData: Array[Byte] = {
-            val len = new File(checksumOutput).length()
-            val buf = new Array[Byte](len.toInt)
-            new RandomAccessFile(checksumOutput, "r").read(buf)
-            buf
-          }
-
-          val partIndex = "(\\d+)\\.dat".r.findFirstIn(outputFile).get.replace(".dat", "").toInt
-
-          (partIndex, stdout, stderr, checksumData)
         }
+      }.collect()
+
+    output.foreach(println)
+
+    val distinctParts = output.map(_._1).distinct.size
+    if (output.size != distinctParts) {
+      println(s"output size ${output.size} != distinct size $distinctParts")
+    }
+
+    val outputMap: Map[Int, String] = output.map(t => (t._1, t._3)).toMap
+    val checksumOut = new NodeLocalRDD[(Int, String, String, String, Array[Byte])](
+      sc, output.size, output.map(_._2).toArray) {
+      override def compute(split: Partition, context: TaskContext) = {
+        val outputFile = outputMap(split.index)
+        val checksumOutput = outputFile.replace(".dat", ".sum")
+        val cmd = s"/root/gensort/64/valsort -o $checksumOutput $outputFile"
+        val (exitCode, stdout, stderr) = Sort.runCommand(cmd)
+
+        val checksumData: Array[Byte] = {
+          val len = new File(checksumOutput).length()
+          val buf = new Array[Byte](len.toInt)
+          new RandomAccessFile(checksumOutput, "r").read(buf)
+          buf
+        }
+
+        Iterator((split.index, outputFile, stdout, stderr, checksumData))
       }
     }.collect()
 
-    output.foreach { case (part, stdout, stderr, _) =>
-      println(s"$part\t$stdout\t$stderr")
+    checksumOut.foreach { case (part, outputFile, stdout, stderr, _) =>
+      println(s"$part\t$outputFile\t$stdout\t$stderr")
     }
 
     val checksumFile = s"/root/sort-${sizeInGB}g-$numParts-out.sum"
     println(s"checksum output: $checksumFile")
     val writer = new FileOutputStream(new File(checksumFile))
-    output.foreach {  case (_, _, _, checksumData: Array[Byte]) =>
+    checksumOut.foreach {  case (_, _, _, _, checksumData: Array[Byte]) =>
       writer.write(checksumData)
     }
     writer.close()
