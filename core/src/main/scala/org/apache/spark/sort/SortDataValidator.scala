c57fdc4ae7d22751ed95f229d325a76c3f95b9b4
Reynold Xin <rxin@apache.org>
2014-09-16 14:59:26 -0700

    New data validator.

--- core/src/main/scala/org/apache/spark/sort/SortDataValidator.scala
+++ core/src/main/scala/org/apache/spark/sort/SortDataValidator.scala
@@ -1,6 +1,6 @@
 package org.apache.spark.sort
 
-import java.io.{FileOutputStream, RandomAccessFile, File}
+import java.io.{FilenameFilter, FileOutputStream, RandomAccessFile, File}
 
 import org.apache.spark.{TaskContext, Partition, SparkConf, SparkContext}
 
@@ -11,54 +11,53 @@ object SortDataValidator {
     val sizeInGB = args(0).toInt
     val numParts = args(1).toInt
     val sc = new SparkContext(new SparkConf())
-    genSort(sc, sizeInGB, numParts)
+    validate(sc, sizeInGB, numParts)
   }
 
-  def genSort(sc: SparkContext, sizeInGB: Int, numParts: Int, numEbsVols: Int = 8): Unit = {
-
-    val sizeInBytes = sizeInGB.toLong * 1000 * 1000 * 1000
-    val numRecords = sizeInBytes / 100
-    val recordsPerPartition = math.ceil(numRecords.toDouble / numParts).toLong
+  def validate(sc: SparkContext, sizeInGB: Int, numParts: Int): Unit = {
 
     val hosts = Sort.readSlaves()
+    val numEBS = UnsafeSort.NUM_EBS
 
-    val output = new NodeLocalRDD[(String, Int, String, String, String, Array[Byte])](sc, numParts, hosts) {
-      override def compute(split: Partition, context: TaskContext) = {
-        val part = split.index
-        val host = split.asInstanceOf[NodeLocalRDDPartition].node
+    val output = sc.parallelize(1 to hosts.length, hosts.length).mapPartitions { iter =>
 
-        val start = recordsPerPartition * part
-        val volIndex = part % numEbsVols
+      (0 until numEBS).iterator.flatMap { ebs =>
+        val baseFolder = new File(s"/vol$ebs/sort-${sizeInGB}g-$numParts-out")
+        val files = baseFolder.listFiles(new FilenameFilter {
+          override def accept(dir: File, filename: String): Boolean = {
+            filename.endsWith(".dat")
+          }
+        })
 
-        val baseFolder = s"/vol$volIndex/sort-${sizeInGB}g-$numParts-out"
-        if (!new File(baseFolder).exists()) {
-          new File(baseFolder).mkdirs()
+        files.iterator.map { file: File =>
+          val outputFile = file.getAbsolutePath
+          val checksumOutput = outputFile.replace(".dat", ".sum")
+          val cmd = s"/root/gensort/64/valsort -o $checksumOutput $outputFile"
+
+          val (exitCode, stdout, stderr) = Sort.runCommand(cmd)
+
+          val checksumData: Array[Byte] = {
+            val len = new File(checksumOutput).length()
+            val buf = new Array[Byte](len.toInt)
+            new RandomAccessFile(checksumOutput, "r").read(buf)
+            buf
+          }
+
+          val partIndex = "(\\d+)\\.dat".r.findFirstIn(outputFile).get.toInt
+
+          (partIndex, stdout, stderr, checksumData)
         }
-
-        val outputFile = s"$baseFolder/part$part.dat"
-        val checksumOutput = s"$baseFolder/part$part.sum"
-        val cmd = s"/root/gensort/64/valsort -o $checksumOutput $outputFile"
-        val (exitCode, stdout, stderr) = Sort.runCommand(cmd)
-
-        val checksumData: Array[Byte] = {
-          val len = new File(checksumOutput).length()
-          val buf = new Array[Byte](len.toInt)
-          new RandomAccessFile(checksumOutput, "r").read(buf)
-          buf
-        }
-
-        Iterator((host, part, outputFile, stdout, stderr, checksumData))
       }
     }.collect()
 
-    output.foreach { case (host, part, outputFile, stdout, stderr, _) =>
-      println(s"$part\t$host\t$outputFile\t$stdout\t$stderr")
+    output.foreach { case (part, stdout, stderr, _) =>
+      println(s"$part\t$stdout\t$stderr")
     }
 
     val checksumFile = s"/root/sort-${sizeInGB}g-$numParts-out.sum"
     println(s"checksum output: $checksumFile")
     val writer = new FileOutputStream(new File(checksumFile))
-    output.foreach {  case (_, _, _, _, _, checksumData: Array[Byte]) =>
+    output.foreach {  case (_, _, _, checksumData: Array[Byte]) =>
       writer.write(checksumData)
     }
     writer.close()
