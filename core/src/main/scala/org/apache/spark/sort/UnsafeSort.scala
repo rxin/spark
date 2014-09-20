18c2a3a87f4ab77ab3e14f497222575882b7f4b0
Reynold Xin <rxin@apache.org>
2014-09-19 21:14:22 -0700

    Added Matei sort.

--- core/src/main/scala/org/apache/spark/sort/UnsafeSort.scala
+++ core/src/main/scala/org/apache/spark/sort/UnsafeSort.scala
@@ -185,6 +185,9 @@ object UnsafeSort extends Logging {
     /** list of pointers to each block, used for sorting. */
     val pointers: Array[Long] = new Array[Long](capacity.toInt)
 
+    /** an array of 2 * capacity longs that we can use for records holding our keys */
+    val keys: Array[Long] = new Array[Long](2 * capacity.toInt)
+
     private[this] val ioBufAddressField = {
       val f = classOf[java.nio.Buffer].getDeclaredField("address")
       f.setAccessible(true)
@@ -293,13 +296,14 @@ object UnsafeSort extends Logging {
         val sortBuffer = sortBuffers.get()
 
         readFileIntoBuffer(inputFile, sortBuffer)
-        buildLongPointers(sortBuffer, fileSize)
+        //buildLongPointers(sortBuffer, fileSize)
 
         // Sort!!!
         {
           val startTime = System.currentTimeMillis
-          val sorter = new Sorter(new LongArraySorter).sort(
-            sortBuffer.pointers, 0, recordsPerPartition.toInt, ord)
+          //val sorter = new Sorter(new LongArraySorter).sort(
+          //  sortBuffer.pointers, 0, recordsPerPartition.toInt, ord)
+          sortWithKeys(sortBuffer, 0, recordsPerPartition.toInt)
           val timeTaken = System.currentTimeMillis - startTime
           logInfo(s"XXX Sorting $recordsPerPartition records took $timeTaken ms")
           println(s"XXX Sorting $recordsPerPartition records took $timeTaken ms")
@@ -345,5 +349,101 @@ object UnsafeSort extends Logging {
      */
     override protected def allocate(length: Int): Array[Long] = new Array[Long](length)
   }
+
+  // Sort a range of a SortBuffer using only the keys, then update the pointers field to match
+  // sorted order. Unlike the other sort methods, this copies the keys into an array of Longs
+  // (with 2 Longs per record in the buffer to capture the 10-byte key and its index) and sorts
+  // them without having to look up random locations in the original data on each comparison.
+  private def sortWithKeys(sortBuf: SortBuffer, start: Int, end: Int) {
+    val keys = sortBuf.keys
+    val pointers = sortBuf.pointers
+    val baseAddress = sortBuf.address
+    import java.lang.Long.reverseBytes
+
+    // Fill in the keys array
+    var i = 0
+    while (i < end - start) {
+      val index = (pointers(start + i) - baseAddress) / 100
+      //assert(index >= 0L && index <= 0xFFFFFFFFL)
+      val headBytes = // First 7 bytes
+        reverseBytes(UNSAFE.getLong(pointers(start + i))) >>> 8
+      val tailBytes = // Last 3 bytes
+        reverseBytes(UNSAFE.getLong(pointers(start + i) + 7)) >>> (8 * 5)
+      keys(2 * i) = headBytes
+      keys(2 * i + 1) = (tailBytes << 32) | index
+      i += 1
+    }
+
+    // Sort it
+    new Sorter(new LongPairArraySorter).sort(keys, 0, end - start, longPairOrdering)
+
+    // Fill back the pointers array
+    i = 0
+    while (i < end - start) {
+      pointers(start + i) = baseAddress + (keys(2 * i + 1) & 0xFFFFFFFFL) * 100
+      i += 1
+    }
+
+    /*
+    // Validate that the data is sorted
+    i = start
+    while (i < end - 1) {
+      assert(ord.compare(pointers(i), pointers(i + 1)) <= 0)
+      i += 1
+    }
+    */
+  }
+
+  final class LongPairArraySorter extends SortDataFormat[(Long, Long), Array[Long]] {
+    /** Return the sort key for the element at the given index. */
+    override protected def getKey(data: Array[Long], pos: Int): (Long, Long) = {
+      (data(2 * pos), data(2 * pos + 1))
+    }
+
+    /** Swap two elements. */
+    override protected def swap(data: Array[Long], pos0: Int, pos1: Int) {
+      var tmp = data(2 * pos0)
+      data(2 * pos0) = data(2 * pos1)
+      data(2 * pos1) = tmp
+      tmp = data(2 * pos0 + 1)
+      data(2 * pos0 + 1) = data(2 * pos1 + 1)
+      data(2 * pos1 + 1) = tmp
+    }
+
+    /** Copy a single element from src(srcPos) to dst(dstPos). */
+    override protected def copyElement(src: Array[Long], srcPos: Int,
+                                       dst: Array[Long], dstPos: Int) {
+      dst(2 * dstPos) = src(2 * srcPos)
+      dst(2 * dstPos + 1) = src(2 * srcPos + 1)
+    }
+
+    /**
+     * Copy a range of elements starting at src(srcPos) to dst, starting at dstPos.
+     * Overlapping ranges are allowed.
+     */
+    override protected def copyRange(src: Array[Long], srcPos: Int,
+                                     dst: Array[Long], dstPos: Int, length: Int) {
+      System.arraycopy(src, 2 * srcPos, dst, 2 * dstPos, 2 * length)
+    }
+
+    /**
+     * Allocates a Buffer that can hold up to 'length' elements.
+     * All elements of the buffer should be considered invalid until data is explicitly copied in.
+     */
+    override protected def allocate(length: Int): Array[Long] = new Array[Long](2 * length)
+  }
+
+  final class LongPairOrdering extends Ordering[(Long, Long)] {
+    override def compare(left: (Long, Long), right: (Long, Long)): Int = {
+      val c1 = java.lang.Long.compare(left._1, right._1)
+      if (c1 != 0) {
+        c1
+      } else {
+        java.lang.Long.compare(left._2, right._2)
+      }
+    }
+  }
+
+  val longPairOrdering = new LongPairOrdering
 
 }
