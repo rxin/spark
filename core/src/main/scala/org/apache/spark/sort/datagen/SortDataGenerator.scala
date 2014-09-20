4c9f1b0b89a195e8aa75dfd33283bc430cf6169d
Reynold Xin <rxin@apache.org>
2014-09-19 17:50:34 -0700

    Added a Java data generator.

---
+++ core/src/main/scala/org/apache/spark/sort/datagen/SortDataGenerator.scala
@@ -0,0 +1,133 @@
+package org.apache.spark.sort.datagen
+
+import java.io.{FileOutputStream, BufferedOutputStream, File}
+import java.util.concurrent.atomic.AtomicInteger
+
+import org.apache.spark.sort.{NodeLocalRDDPartition, NodeLocalRDD, Sort}
+import org.apache.spark.{TaskContext, Partition, SparkConf, SparkContext}
+
+
+object SortDataGenerator {
+
+  private[this] val numTasksOnExecutor = new AtomicInteger
+
+  def main(args: Array[String]): Unit = {
+    val sizeInGB = args(0).toInt
+    val numParts = args(1).toInt
+    val sc = new SparkContext(
+      new SparkConf().setAppName(s"DataGenerator - $sizeInGB GB - $numParts partitions"))
+    genSort(sc, sizeInGB, numParts)
+  }
+
+  def genSort(sc: SparkContext, sizeInGB: Int, numParts: Int, numEbsVols: Int = 8): Unit = {
+    val sizeInBytes = sizeInGB.toLong * 1000 * 1000 * 1000
+    val numRecords = sizeInBytes / 100
+    val recordsPerPartition = math.ceil(numRecords.toDouble / numParts).toLong
+
+    val hosts = Sort.readSlaves()
+
+    val output = new NodeLocalRDD[(String, Int, String)](sc, numParts, hosts) {
+      override def compute(split: Partition, context: TaskContext) = {
+        val part = split.index
+        val host = split.asInstanceOf[NodeLocalRDDPartition].node
+
+        val start = recordsPerPartition * part
+        val volIndex = numTasksOnExecutor.getAndIncrement() % numEbsVols
+
+        val baseFolder = s"/vol$volIndex/sort-${sizeInGB}g-$numParts"
+        if (!new File(baseFolder).exists()) {
+          new File(baseFolder).mkdirs()
+        }
+
+        val outputFile = s"$baseFolder/part$part.dat"
+
+        val os = new BufferedOutputStream(new FileOutputStream(outputFile), 4 * 1024 * 1024)
+
+        val one = new Unsigned16(1)
+        val firstRecordNumber = new Unsigned16(part.toLong * recordsPerPartition)
+        val recordsToGenerate = new Unsigned16(recordsPerPartition)
+        val recordNumber = new Unsigned16(firstRecordNumber)
+        val lastRecordNumber = new Unsigned16(firstRecordNumber)
+        lastRecordNumber.add(recordsToGenerate)
+
+        val rand = Random16.skipAhead(firstRecordNumber)
+        val buf: Array[Byte] = new Array[Byte](100)
+
+        var i = 0
+        while (i < recordsPerPartition) {
+          Random16.nextRand(rand)
+          generateRecord(buf, rand, recordNumber)
+          os.write(buf)
+          recordNumber.add(one)
+          i += 1
+        }
+
+        os.close()
+
+        Iterator((host, part, outputFile))
+      }
+    }.collect()
+
+    output.foreach { case (host, part, outputFile) =>
+      println(s"$part\t$host\t$outputFile")
+    }
+
+    val checksumFile = s"/root/sort-${sizeInGB}g-$numParts.output"
+    println(s"checksum output: $checksumFile")
+    val writer = new java.io.PrintWriter(new File(checksumFile))
+    output.foreach {  case (host, part, outputFile) =>
+      writer.write(s"$part\t$host\t$outputFile")
+    }
+    writer.close()
+  }  // end of genSort
+
+  /**
+   * Generate a binary record suitable for all sort benchmarks except PennySort.
+   *
+   * @param recBuf record to return
+   */
+  def generateRecord(recBuf: Array[Byte], rand: Unsigned16, recordNumber: Unsigned16): Unit = {
+    // Generate the 10-byte key using the high 10 bytes of the 128-bit random number
+    var i = 0
+    while (i < 10) {
+      recBuf(i) = rand.getByte(i)
+      i += 1
+    }
+
+    // Add 2 bytes of "break"
+    recBuf(10) = 0x00.toByte
+    recBuf(11) = 0x11.toByte
+
+    // Convert the 128-bit record number to 32 bits of ascii hexadecimal
+    // as the next 32 bytes of the record.
+    i = 0
+    while (i < 32) {
+      recBuf(12 + i) = recordNumber.getHexDigit(i).toByte
+      i += 1
+    }
+
+    // Add 4 bytes of "break" data
+    recBuf(44) = 0x88.toByte
+    recBuf(45) = 0x99.toByte
+    recBuf(46) = 0xAA.toByte
+    recBuf(47) = 0xBB.toByte
+
+    // Add 48 bytes of filler based on low 48 bits of random number
+    i = 0
+    while (i < 12) {
+      val v = rand.getHexDigit(20 + i).toByte
+      recBuf(48 + i * 4) = v
+      recBuf(49 + i * 4) = v
+      recBuf(50 + i * 4) = v
+      recBuf(51 + i * 4) = v
+      i += 1
+    }
+
+    // Add 4 bytes of "break" data
+    recBuf(96) = 0xCC.toByte
+    recBuf(97) = 0xDD.toByte
+    recBuf(98) = 0xEE.toByte
+    recBuf(99) = 0xFF.toByte
+  }
+
+}
