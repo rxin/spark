a9836cae0856a5adf6fd2ec5564d9537e041cc42
Reynold Xin <rxin@apache.org>
2014-10-02 21:48:39 -0700

    sample per partition

--- core/src/main/scala/org/apache/spark/sort/DaytonaSort.scala
+++ core/src/main/scala/org/apache/spark/sort/DaytonaSort.scala
@@ -266,7 +266,7 @@ object DaytonaSort extends Logging {
 
     {
       val startTime = System.currentTimeMillis()
-      val samplePerPartition = 79
+      val samplePerPartition = new SparkConf().getInt("spark.samplePerPartition", 97)
       val numSampleKeys = numParts * samplePerPartition
       val sampleKeys = new NodeLocalReplicaRDD[Array[Byte]](sc, numParts, replicatedHosts) {
         override def compute(split: Partition, context: TaskContext) = {
