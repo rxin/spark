e224602256dcbf3fa256ea682d91408dfd7935a1
Ralph Furmaniak <rcf@acm.org>
2015-01-06 02:24:21 -0800

    Fix up style, and remove caching/materialization.

--- graphx/src/main/scala/org/apache/spark/graphx/lib/HITS.scala
+++ graphx/src/main/scala/org/apache/spark/graphx/lib/HITS.scala
@@ -20,8 +20,8 @@ package org.apache.spark.graphx.lib
 import scala.reflect.ClassTag
 
 import org.apache.spark.Logging
-import org.apache.spark.graphx._
-import org.apache.spark.rdd._
+import org.apache.spark.graphx.{Graph, TripletFields, VertexRDD}
+import org.apache.spark.rdd.RDD
 
 /**
  * HITS algorithm implementation.
@@ -64,21 +64,19 @@ object HITS extends Logging {
    *
    */
   def run[VD: ClassTag, ED: ClassTag](
-      graph: Graph[VD, ED], numIter: Int): Graph[(Double, Double), ED] =
-  {
+      graph: Graph[VD, ED], numIter: Int): Graph[(Double, Double), ED] = {
 
     // Scale down non-zero vectors to have norm 1
     def scalingFactor(v: VertexRDD[Double]): Double = {
       // Use fold instead of sum, since sum calls reduce and reduce cannot handle empty lists
-      val norm = Math.sqrt(v.values.map(x => x*x ).fold(0.0)(_ + _))
-      if(norm == 0.0) 1.0
-      else norm
+      val norm = Math.sqrt(v.values.map(x => x * x).fold(0.0)(_ + _))
+      if (norm == 0.0) 1.0 else norm
     }
 
     // Initialize the HITS graph with each edge attribute having
     // its original attribute and each vertex with attribute 1.0
-    var hubGraph = graph.mapVertices( (id, attr) => 1.0 ).cache
-    var authGraph = graph.mapVertices( (id, attr) => 1.0 ).cache
+    var hubGraph = graph.mapVertices((id, attr) => 1.0).cache()
+    var authGraph = graph.mapVertices((id, attr) => 1.0).cache()
 
     // RDD[Double] is more efficient here, hence the seperate graphs
     // Technically there is no need to rescale at every single iteration,
@@ -89,7 +87,7 @@ object HITS extends Logging {
       // First compute the new auth values.
       // Requires a shuffle for aggregation.
       val authUpdates = hubGraph.aggregateMessages[Double](
-        ctx => ctx.sendToDst(ctx.srcAttr), _ + _, TripletFields.Src).cache
+        ctx => ctx.sendToDst(ctx.srcAttr), _ + _, TripletFields.Src).cache()
       val authScale = scalingFactor(authUpdates)
 
       // Forget the old auth values, update to the new values, and unpersist the update RDD.
@@ -98,26 +96,22 @@ object HITS extends Logging {
       authGraph.unpersistVertices(false)
       authGraph = graph.outerJoinVertices(authUpdates) {
         (id, auth, msgSum) => msgSum.getOrElse(0.0)/authScale
-      }.cache()
-      authGraph.edges.foreachPartition(x => {}) // also materializes hitsGraph.vertices
+      }
       authUpdates.unpersist(false)
-
-      logInfo(s"HITS finished first half of iteration $iteration.")
 
       // Next compute the new hub values
       val hubUpdates = authGraph.aggregateMessages[Double](
-        ctx => ctx.sendToSrc(ctx.dstAttr), _ + _, TripletFields.Dst).cache
+        ctx => ctx.sendToSrc(ctx.dstAttr), _ + _, TripletFields.Dst).cache()
       val hubScale = scalingFactor(hubUpdates)
 
       // Forget the old hub values, update to the new values, and unpersist the update RDD
       hubGraph.unpersistVertices(false)
       hubGraph = graph.outerJoinVertices(hubUpdates) {
         (id, hub, msgSum) => msgSum.getOrElse(0.0)/hubScale
-      }.cache()
-      hubGraph.edges.foreachPartition(x => {}) // also materializes hitsGraph.vertices
+      }
       hubUpdates.unpersist(false)
 
-      logInfo(s"HITS finished second half of iteration $iteration.")
+      logInfo(s"HITS finishing iteration $iteration.")
 
       iteration += 1
     }
--- graphx/src/test/scala/org/apache/spark/graphx/lib/HITSSuite.scala
+++ graphx/src/test/scala/org/apache/spark/graphx/lib/HITSSuite.scala
@@ -36,7 +36,7 @@ object GridHITS {
     def sub2ind(r: Int, c: Int): Int = r * nCols + c
     // scale an array to have length 1
     def normalize(arr: Array[Double]): Array[Double] = {
-      val normArr = Math.sqrt(arr.map(x => x*x).sum)
+      val normArr = Math.sqrt(arr.map(x => x * x).sum)
       arr.map(x => x / normArr)
     }
     // Make the grid graph
@@ -56,10 +56,10 @@ object GridHITS {
     var auth = Array.fill(nRows * nCols)(1.0)
     for (iter <- 0 until nIter) {
       for (ind <- 0 until (nRows * nCols))
-        auth(ind) = inNbrs(ind).map( nbr => hub(nbr)).sum
+        auth(ind) = inNbrs(ind).map(nbr => hub(nbr)).sum
       auth = normalize(auth)
       for (ind <- 0 until (nRows * nCols))
-        hub(ind) = outNbrs(ind).map( nbr => auth(nbr)).sum
+        hub(ind) = outNbrs(ind).map(nbr => auth(nbr)).sum
       hub = normalize(hub)
     }
     (0L until (nRows * nCols)).zip(hub.zip(auth))
@@ -71,7 +71,7 @@ object GridHITS {
 class HITSSuite extends FunSuite with LocalSparkContext {
 
   def compareHits(a: VertexRDD[(Double,Double)], b: VertexRDD[(Double,Double)]): Double = {
-    a.leftJoin(b){ (id, a, b) => Math.abs(a._1 - b.get._1) + Math.abs(a._2 - b.get._2) }.values.sum
+    a.leftJoin(b) { (id, a, b) => Math.abs(a._1 - b.get._1) + Math.abs(a._2 - b.get._2) } .values.sum
   }
 
   // Compare result on Star Shaped graph with the theoretical value
@@ -81,12 +81,12 @@ class HITSSuite extends FunSuite with LocalSparkContext {
       val errorTol = 1.0e-5
 
       val hits1 = starGraph.staticHITS(numIter = 1).vertices.cache()
-      val hits2 = starGraph.staticHITS(numIter = 2).vertices.cache()
+      val hits2 = starGraph.staticHITS(numIter = 2).vertices
 
       val referenceHits = starGraph.mapVertices ( (id,attr) =>
           if (id == 0) (0.0, 1.0)
           else (0.0625, 0.0)
-      ).vertices.cache()
+      ).vertices
 
       assert(compareHits(referenceHits, hits1) < errorTol)
       assert(compareHits(hits1, hits2) < errorTol)
@@ -116,18 +116,18 @@ class HITSSuite extends FunSuite with LocalSparkContext {
   // Compare result on Chain graph (10 vertices connected in a row) with the theoretical value
   test("Chain HITS") {
     withSpark { sc =>
-      val rawEdges = sc.parallelize((0L until 9L).map(x => (x, x+1) ))
+      val rawEdges = sc.parallelize((0L until 9L).map(x => (x, x+1)))
       val chain = Graph.fromEdgeTuples(rawEdges, 1.0).cache()
       val numIter = 10
       val errorTol = 1.0e-5
 
       val staticHits = chain.staticHITS(numIter).vertices
 
-      val referenceHits = chain.mapVertices ( (id,attr) => id match {
+      val referenceHits = chain.mapVertices((id,attr) => id match {
           case 0 => (1/3.0, 0.0)
           case 9 => (0.0, 1/3.0)
           case _ => (1/3.0, 1/3.0)
-      }).vertices
+      } ).vertices
 
       assert(compareHits(referenceHits, staticHits) < errorTol)
     }
@@ -137,13 +137,13 @@ class HITSSuite extends FunSuite with LocalSparkContext {
   // Make sure that the result on an empty graph is all zero
   test("Empty HITS") {
     withSpark { sc =>
-      val graph = GraphGenerators.starGraph(sc, 10).subgraph(e => false).cache
+      val graph = GraphGenerators.starGraph(sc, 10).subgraph(e => false).cache()
       val numIter = 25
       val errorTol = 1.0e-5
 
       val staticHits = graph.staticHITS(numIter).vertices.cache
 
-      val referenceHits = graph.mapVertices ( (id,attr) => (0.0, 0.0) ).vertices
+      val referenceHits = graph.mapVertices((id,attr) => (0.0, 0.0)).vertices
 
       assert(compareHits(referenceHits, staticHits) < errorTol)
     }
@@ -153,18 +153,18 @@ class HITSSuite extends FunSuite with LocalSparkContext {
   // Compare result on a graph composed of multiple cycles and a chain, with the theoretical value
   test("Cycle Forest HITS") {
     withSpark { sc =>
-      val rawEdges = sc.parallelize((0L until 64).map(x => (x, x/10*10 + (x+1)%10) ))
+      val rawEdges = sc.parallelize((0L until 64).map(x => (x, x/10*10 + (x+1)%10)))
       val graph = Graph.fromEdgeTuples(rawEdges, 1.0).cache()
       val numIter = 15
       val errorTol = 1.0e-5
 
-      val staticHits = graph.staticHITS(numIter).vertices.cache
+      val staticHits = graph.staticHITS(numIter).vertices
 
-      val referenceHits = graph.mapVertices ( (id,attr) => id match {
+      val referenceHits = graph.mapVertices((id,attr) => id match {
           case 60 => (0.125, 0.0)
           case 64 => (0.0, 0.125) 
           case _ => (0.125, 0.125)
-      }).vertices
+      } ).vertices
 
       assert(compareHits(referenceHits, staticHits) < errorTol)
     }
